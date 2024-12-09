5e6b892fcd3de2856effa6a2777cedc53b718fce
Matthew R. Leclerc <matthew.leclerc1@snhu.edu>
2024-12-09 17:22:51 -0500

    Module 6 completed baseline

--- app.js
+++ app.js
@@ -7,19 +7,30 @@ var logger = require('morgan');
 var apiRouter = require('./app_api/routes/index');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
-var travelRouter = require('./app_server/routes/travel')
+var travelRouter = require('./app_server/routes/travel');
 
 var handlebars = require('hbs');
 
-//Bring in the DB
-require('./app_api/models/db')
+// Bring in the DB
+require('./app_api/models/db');
 
 var app = express();
 
-// view engine setup
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200'); // Allow requests from Angular app
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Credentials', 'true'); // Optional: if cookies or auth headers are sent
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end(); // Handle preflight requests
+  }
+  next();
+});
+
+// View engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 
-// register handlebars partials (https://www.npmjs.com/package/hbs)
+// Register handlebars partials (https://www.npmjs.com/package/hbs)
 handlebars.registerPartials(__dirname + '/app_server/views/partials');
 
 app.set('view engine', 'hbs');
@@ -30,25 +41,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// wire up routes to the controllers
+// Wire up routes to the controllers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
 app.use('/api', apiRouter);
 
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+// Error handler
+app.use(function (err, req, res, next) {
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -6,11 +6,14 @@ const tripsController = require('../controllers/trips');
 
 // Define route for our trips endpoint
 router
-    .route('/trips')
-    .get(tripsController.tripsList); // GET Method routes tripsList
+  .route('/trips')
+  .get(tripsController.tripsList) // GET Method routes tripList
+  .post(tripsController.tripsAddTrip); // POST Method Adds a Trip
 
+// GET Method routes tripsFindByCode - requires parameter
 router
-    .route('/trips/:tripCode')
-    .get(tripsController.tripsFindByCode); // GET Method routes tripsList
+  .route('/trips/:tripCode')
+  .get(tripsController.tripsFindByCode)
+  .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
