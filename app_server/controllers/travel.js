621783c0789d6e2b1406597980444743c7870f3c
Matthew R. Leclerc <matthew.leclerc1@snhu.edu>
2024-12-09 14:13:03 -0500

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,9 +1,24 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways' });
-  };
-  
-  module.exports = {
-    travel
-  };
-  
+  res.render('travel', { title: 'Travlr Getaways', trips });
+};
+
+module.exports = {
+  travel
+};
+const fs = require('fs');
+const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+const mainPage = JSON.parse(fs.readFileSync('app_server/data/index.json', 'utf8'));
+
+/* GET home page. */
+const index = (req, res) => {
+    pageTitle = packageJson.description + ' | Home';
+    res.render('index', { activePage: 'main', title: pageTitle, mainPage });
+};
+
+module.exports = {
+    index
+};
